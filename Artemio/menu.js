b20a4a7e4daedf854b1a1c2374879701bc32c70d
Artemio <34037814+webstudiotest1@users.noreply.github.com>
2019-04-15 00:42:42 +0300

    Place page
    Artemio. Place page

---
+++ Artemio/menu.js
@@ -0,0 +1,8 @@
+function barFunction() {
+  var x = document.getElementsByClassName("menu__icon");
+  if (x.style.display === "block") {
+  x.style.display = "none";
+  } else {
+  x.style.display = "block";
+  }
+}
